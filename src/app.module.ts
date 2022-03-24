6942ed9b6a2816eb32c74772fdb3bbb15282ec19
Josh Mo <joshmomel@gmail.com>
2022-03-24 22:14:59 +1100

    feat: add sql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AllExceptionsFilter } from './all-exceptions.filter';
 import { CatsModule } from './cats/cats.module';
 import { LoggingInterceptor } from './logging.interceptor';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    CatsModule,
+  ],
   providers: [
     {
       provide: APP_FILTER,
